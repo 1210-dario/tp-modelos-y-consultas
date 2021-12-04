5e5fe6647c08045abffd7844cd6a386bd54f8b62
Dario Castromonte <dario.castromonte@gmail.com>
2021-12-04 14:00:10 -0300

    finish tp modelos y consultas

---
+++ src/controllers/moviesController.js
@@ -0,0 +1,48 @@
+const db = require('../database/models');
+
+const moviesController = {
+    'list': (req, res) => {
+        db.Movie.findAll({
+            include: ['genre']
+        })
+            .then(movies => {
+                res.render('moviesList.ejs', { movies })
+            })
+    },
+    'detail': (req, res) => {
+        db.Movie.findByPk(req.params.id,
+            {
+                include: ['genre']
+            })
+            .then(movie => {
+                res.render('moviesDetail.ejs', { movie });
+            });
+    },
+    'new': (req, res) => {
+        db.Movie.findAll({
+            order: [
+                ['release_date', 'DESC']
+            ],
+            limit: 5
+        })
+            .then(movies => {
+                res.render('newestMovies', { movies });
+            });
+    },
+    'recomended': (req, res) => {
+        db.Movie.findAll({
+            include: ['genre'],
+            where: {
+                rating: { [db.Sequelize.Op.gte]: 8 }
+            },
+            order: [
+                ['rating', 'DESC']
+            ]
+        })
+            .then(movies => {
+                res.render('recommendedMovies.ejs', { movies });
+            });
+    },
+}
+
+module.exports = moviesController;
